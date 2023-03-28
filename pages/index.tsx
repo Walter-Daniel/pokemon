1bd4e9de586b6f859878d90133d36df338be8ada
Walter <walterdcarrizo.19@gmail.com>
2023-03-28 20:51:06 -0300

    instalaciones de next iu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-
+import { Button } from '@nextui-org/react';
 
 
 const HomePage = () => {
+   
+
   return (
     <>
       <h1>Hola mundo!!!</h1>
+      <Button color="gradient">Click me</Button>;
     </>
   ) 
 }
